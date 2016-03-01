90bca7c6f572ed5bf4deace7cabe717548787eab
Emery Medley <russ@rmedley.com>
2016-03-01 13:42:12 -0500

    first commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,104 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ //Bio Object
+
+var bio = {
+	
+ "name" : "Russell Medley",
+ "role" : "Web Developer",
+	 "contacts" : {
+	 	"mobile" : "304.777.8408",
+	 	"email" : "medleyr3@gmail.com",
+	 	"location" : "Pittsburgh",
+	 	"github" : "medleyr3",
+	 	"twitter" : "medleyr3"
+	 			  },
+
+
+ "welcomeMessage" : "Hello, my name is Russell. I live in Pittsburgh. My favorite way to work is in the morning when I won't be distracted. In my free time, I like to make videos, music, and draw.",
+ 	"skills" : ["Javascript", "Html", "CSS", "Github"],
+     "bio_pic" : "images/russ300x300.png"
+}
+
+
+
+//educations object
+
+var education = {
+
+	"schools": [
+	{
+		"name": "Univesity Of Phoenix",
+		"location": "Phoenix, AZ",
+		"degree": "MBA",
+		"major": ["Accounting"],
+		"date" : "2008",
+		"url" : "http://www.uop.edu"
+
+	},
+	 {
+		"name": "Norfolk State",
+		"location": "Norfolk, Va",
+		"degree": "BA",
+		"majors": ["Interdisciplinary Studies"],
+		"dates" : "1999",
+		"url" : "http://www.nsu.edu"
+	}],
+   
+   "online_courses" : {
+   	"onineCourse" : [
+{
+        "title": "Javascript Basics",
+		"school": "Udacity.com",
+		"dates" : "2016",
+		"url" : "http://Udacity.com"
+}
+]
+
+}
+}
+
+//Work object
+
+var work = {
+ "jobs" : [
+ {
+"employer" :  "Russell Coders", 	
+"title": "Lead Developer",
+"location" : "Morgantown",
+"dates" : "2007 - Present",
+"projectDescription" : "freelancer of web projects"
+},
+{
+"employer" :  "IRISMAGIC", 	
+"title": "Web Designer/Application Developer",
+"location" : "Morgantown",
+"dates" : "2007 - Present",
+"projectDescription" : "Design Apps and Build,Maintain the website"
+}
+
+]
+}
+
+//projects object
+
+var projects = { 
+
+	"project" : [
+
+{
+"projectName" : "Portfolio Site",
+"projectDates" : "2016 ",
+"projectDescription" : "Basic Porfolio Site",
+"projectImage" : "images/porfolio/p1.svg"
+},
+{
+"projectName": "Resume Site",
+"projectDates" : "2016",
+"projectDescription" :"An advanced resume site using Javascript",
+"projectImage" : "images/porfolio/p2.svg"
+}
+	]
+}
